ee81d193decef4bc6dd6652271eb1b60b5648756
youssefERG <erengamer555@gmail.com>
2025-12-18 17:52:52 +0100

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,9 +8,13 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+    int left, right;
 
-	return ((int)binary_tree_height(tree->left)
-		- (int)binary_tree_height(tree->right));
+    if (!tree)
+        return (0);
+
+    left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+    right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+
+    return (left - right);
 }
